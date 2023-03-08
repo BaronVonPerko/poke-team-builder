60894f6543ee59a86827c78d76712889098a92d7
Chris Perko <chris@chrisperko.net>
2023-03-08 12:04:29 -0500

    Added angular app poke-team-builder

---
+++ apps/poke-team-builder/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'poke-root',
+  template: ` <p>test</p> `,
+  styles: [],
+})
+export class AppComponent {
+}
