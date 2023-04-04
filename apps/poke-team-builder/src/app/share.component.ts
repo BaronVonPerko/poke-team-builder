bbfa84964ef0cd542cb45d5babf50de581bf71c0
Chris Perko <chris@chrisperko.net>
2023-04-04 17:48:25 -0400

    Added snackbar on share click

--- apps/poke-team-builder/src/app/share.component.ts
+++ apps/poke-team-builder/src/app/share.component.ts
@@ -1,12 +1,13 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import Pokemon from './models/pokemon';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'poke-share',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatSnackBarModule, MatIconModule],
   template: `<button mat-raised-button color="primary" (click)="share()">
     <mat-icon>share</mat-icon>
     Share
@@ -16,9 +17,12 @@ import Pokemon from './models/pokemon';
 export class ShareComponent {
   @Input() team: Pokemon[] = [];
 
+  snackbar = inject(MatSnackBar);
+
   share() {
     const ids = this.team.map(pokemon => pokemon.url.split('/')[6]);
     const url = `http://localhost:4200?team=${ids.join(',')}`;
     navigator.clipboard.writeText(url);
+    this.snackbar.open('Copied to clipboard!', 'Close', {duration: 2000});
   }
 }
